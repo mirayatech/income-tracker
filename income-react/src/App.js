29bff7317a884947759b00d9655027019e73c0e6
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-19 08:07:38 +0200

    create header

--- income-react/src/App.js
+++ income-react/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
 
 function App() {
   // Array of incomes
---
+++ income-react/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+
+function Header({ totalIncome }) {
+  return (
+    <header>
+      <h1>Income Tracker</h1>
+      <div className="total-income">
+        {" "}
+        <FontAwesomeIcon icon={faDollarSign} />
+        {totalIncome}
+      </div>
+    </header>
+  );
+}
+
+export default Header;
